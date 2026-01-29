9387fe6af2944686b2cb8b7ce66c61df96335cc8
Brandd_dev <98653274+UnderWorksDigi@users.noreply.github.com>
2026-01-29 13:03:10 +0000

    Ready

--- backend/src/routes/webhook.routes.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-
-const { handleStripeWebhook } = require("../controllers/webhook.controller");
-
-router.post("/", handleStripeWebhook);
-
-module.exports = router;
--- backend/src/services/whatsapp.service.js
+++
@@ -1,9 +0,0 @@
-exports.sendWhatsAppTicket = async (order) => {
-  // This is where WhatsApp Cloud API will go later
-  // For now, we just log
-
-  console.log("WhatsApp ticket delivery queued:", {
-    orderId: order.id,
-    email: order.email
-  });
-};
